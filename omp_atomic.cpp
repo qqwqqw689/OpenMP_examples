e3fd871cf36cde1e2da2366e9a34d45855110cdb
qqwqqw689 <114795525+qqwqqw689@users.noreply.github.com>
2023-10-08 20:38:41 +0800

    Create omp_atomic.cpp

---
+++ omp_atomic.cpp
@@ -0,0 +1,16 @@
+// omp_atomic.cpp
+// compile with: /openmp
+#include <stdio.h>
+#include <omp.h>
+
+#define MAX 10
+
+int main() {
+   int count = 0;
+   #pragma omp parallel num_threads(MAX)
+   {
+      #pragma omp atomic
+      count++;
+   }
+   printf_s("Number of threads: %d\n", count);
+}
