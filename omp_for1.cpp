637cbea414ffc893a92d7546971b4a5bfad8d3a8
qqwqqw689 <114795525+qqwqqw689@users.noreply.github.com>
2023-12-30 21:46:20 +0800

    updates

--- omp_for1.cpp
+++ omp_for1.cpp
@@ -3,12 +3,14 @@
 
 int main()
 {
-	omp_set_num_threads(100);
+    omp_set_num_threads(1000);
 	#pragma omp parallel for
-	for(int i=0; i < 100; i++)
-	{
-		printf("i: %d, threadid: %d\n", i, omp_get_thread_num());
-	}
-
+	    for(int i=0; i < 100; i++)
+	    {
+	    	printf("i: %d, threadid: %d\n", i, omp_get_thread_num());
+	    }
+    
+    printf("max_threads: %d\n", omp_get_max_threads());
+    
 	return 0;
 }
